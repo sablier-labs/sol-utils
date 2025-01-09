f0494ae4a0a2a9f5a758f420bdfec2cbd99dc039
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-01-09 22:37:10 +0200

    feat: implement SablierFees contract

---
+++ src/SablierFees.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.22;
+
+import { ISablierFees } from "./interfaces/ISablierFees.sol";
+import { Errors } from "./libraries/Errors.sol";
+
+import { Adminable } from "./Adminable.sol";
+
+/// @title SablierFees
+/// @notice See the documentation in {ISablierFees}.
+abstract contract SablierFees is Adminable, ISablierFees {
+    /// @inheritdoc ISablierFees
+    function collectFees() external override {
+        uint256 feeAmount = address(this).balance;
+
+        // Effect: transfer the fees to the admin.
+        (bool success,) = admin.call{ value: feeAmount }("");
+
+        // Revert if the call failed.
+        if (!success) {
+            revert Errors.SablierFees_FeeTransferFail(admin, feeAmount);
+        }
+
+        // Log the fee withdrawal.
+        emit ISablierFees.CollectFees({ admin: admin, feeAmount: feeAmount });
+    }
+}
